c8f890e3b7a851e39bf54fd49f5f0e42199ffcc1
hesch <henri@reschmi.de>
2021-10-30 00:57:37 +0200

    move pyodide location to inside src folder

--- postinstall.sh
+++ postinstall.sh
@@ -6,7 +6,7 @@ VERSION="0.18.1"
 ARCHIVE_PATH="dist"
 ARCHIVE_FILENAME="pyodide.tar.bz2"
 ARCHIVE_FULL_PATH="$ARCHIVE_PATH/$ARCHIVE_FILENAME"
-OUT="pyodide"
+OUT="src/pyodide"
 
 echo "Installing Pyodide in Version $VERSION"
 
@@ -18,7 +18,7 @@ fi
 
 if ! [ -d $OUT ]; then
   echo "Extracting Pyodide files into $OUT"
-  tar -xf $ARCHIVE_FULL_PATH
+  tar -xf $ARCHIVE_FULL_PATH -C src
 fi
 
 echo "Pyodide install complete!"
