c96ace11dad850bc3cfc2c018392ff6da6e40b50
hesch <henri@reschmi.de>
2021-10-30 00:25:25 +0200

    fix postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -3,18 +3,22 @@
 set -euf -o pipefail
 
 VERSION="0.18.1"
-ARCHIVE_PATH="dist/pyodide.tar.bz2"
+ARCHIVE_PATH="dist"
+ARCHIVE_FILENAME="pyodide.tar.bz2"
+ARCHIVE_FULL_PATH="$ARCHIVE_PATH/$ARCHIVE_FILENAME"
 OUT="pyodide"
 
 echo "Installing Pyodide in Version $VERSION"
 
-if ! [ -f $ARCHIVE_PATH ]; then
-  curl -o $ARCHIVE_PATH -L https://github.com/pyodide/pyodide/releases/download/$VERSION/pyodide-build-$VERSION.tar.bz2
+mkdir -p $ARCHIVE_PATH
+
+if ! [ -f ARCHIVE_FULL_PATH ]; then
+  curl -o ARCHIVE_FULL_PATH -L https://github.com/pyodide/pyodide/releases/download/$VERSION/pyodide-build-$VERSION.tar.bz2
 fi
 
 if ! [ -d $OUT ]; then
   echo "Extracting Pyodide files into $OUT"
-  tar -xf $ARCHIVE_PATH
+  tar -xf ARCHIVE_FULL_PATH
 fi
 
 echo "Pyodide install complete!"
