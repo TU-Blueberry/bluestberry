38e71d410f560982ab865e7bfef559b0e437ff00
janagoe <goedeke.jana@gmail.com>
2021-11-09 16:08:45 +0100

    Image Viewer refinements

--- src/app/viewer/image-viewer/image-viewer.component.ts
+++ src/app/viewer/image-viewer/image-viewer.component.ts
@@ -1,36 +1,98 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  HostListener
+} from '@angular/core';
 
 @Component({
   selector: 'app-image-viewer',
   templateUrl: './image-viewer.component.html',
-  styleUrls: ['./image-viewer.component.scss']
+  styleUrls: ['./image-viewer.component.scss'],
 })
 export class ImageViewerComponent implements AfterViewInit {
 
-  @ViewChild('imageContainer', {read: ElementRef, static: false}) imageContainer: ElementRef;
-  @ViewChild('imageLoaded', {read: ElementRef, static: false}) imageLoaded: ElementRef;
+  @ViewChild('imageContainer', { read: ElementRef, static: false })
+  imageContainer: ElementRef;
+
+  @ViewChild('imageLoaded', { read: ElementRef, static: false })
+  imageLoaded: ElementRef;
 
   @Input()
-  get imagePath(): string { return this._image_path; }
+  get imagePath(): string {
+    return this._image_path;
+  }
   set imagePath(path: string) {
     this._image_path = path || 'assets/blueberry.png';
   }
 
-  // get imageWidth(): number { 
-  //   this.calculateImageDimensions();
-  //   return this._image_width;
-  // }
-  // set imageWidth(width: number) {
-  //   this._image_width = width || 100;
-  // }
+  constructor() {}
 
-  // get imageHeight(): number { 
-  //   this.calculateImageDimensions();
-  //   return this._image_height;
-  // }
-  // set imageHeight(width: number) {
-  //   this._image_height = width || 100;
-  // }
+  ngAfterViewInit(): void {
+    this._original_image_width = this.imageLoaded.nativeElement.naturalWidth;
+    this._original_image_height = this.imageLoaded.nativeElement.naturalHeight;
+    this._original_image_ratio =
+      this._original_image_width / this._original_image_height;
+
+    this._image_width = this._original_image_width;
+    this._image_height = this._original_image_height;
+
+    if (this._image_height > 0 && this._image_width > 0) {
+      this.onResize();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this._container_width = this.imageContainer.nativeElement.offsetWidth;
+    this._container_height = this.imageContainer.nativeElement.offsetHeight;
+
+    this.calculateImageDimensions();
+
+    this.imageLoaded.nativeElement.width = this._image_width;
+    this.imageLoaded.nativeElement.height = this._image_height;
+  }
+
+  calculateImageDimensions(): void {
+    if (this._image_width > this._container_width) {
+      this._image_width = this._container_width;
+      this._image_height = this._image_width / this._original_image_ratio;
+    }
+
+    if (this._image_height > this._container_height) {
+      this._image_height = this._container_height;
+      this._image_width = this._original_image_ratio * this._image_height;
+    }
+  }
+
+  get hoverMode(): string {
+    if (this._zoom_in) {
+      return 'zoom-in';
+    }
+    return 'zoom-out';
+  }
+
+  @HostListener('window:keydown.control', ['$event'])
+  onKeydownEvent($event: KeyboardEvent) {
+    if (this._hover_img) {
+      this._zoom_in = false;
+    }
+  }
+
+  @HostListener('window:keyup.control', ['$event'])
+  onKeyupEvent($event: KeyboardEvent) {
+    this._zoom_in = true;
+  }
+  
+  mouseOver(event: any): void {
+    this._hover_img = true;
+  }
+
+  mouseOut(event: any): void {
+    this._hover_img = false;
+  }
 
   private _image_path = '';
   private _image_width = 50;
@@ -43,42 +105,6 @@ export class ImageViewerComponent implements AfterViewInit {
   private _original_image_height = 0;
   private _original_image_ratio = 0;
 
-  constructor() { }
-
-
-  ngAfterViewInit(): void {
-
-    this._container_width = this.imageContainer.nativeElement.offsetWidth;
-    this._container_height = this.imageContainer.nativeElement.offsetHeight;
-
-    this._original_image_width = this.imageLoaded.nativeElement.naturalWidth;
-    this._original_image_height = this.imageLoaded.nativeElement.naturalHeight;
-    this._original_image_ratio = this._original_image_width / this._original_image_height;
-
-    this._image_width = this._original_image_width;
-    this._image_height = this._original_image_height;
-  
-    this.calculateImageDimensions();
-
-    this.imageLoaded.nativeElement.width = this._image_width;
-    this.imageLoaded.nativeElement.height = this._image_height;
-
-  }
-  
-  calculateImageDimensions(): void {
-
-    if(this._image_width > this._container_width) {
-      console.log("h1");
-      this._image_width = this._container_width;
-      this._image_height = this._image_width / this._original_image_ratio;
-    }
-    
-    if(this._image_height > this._container_height) {
-      console.log("h2");
-      this._image_height = this._container_height;
-      this._image_width = this._original_image_ratio * this._image_height;
-    }
-
-  }
-
+  private _zoom_in = true;
+  private _hover_img = false;
 }
