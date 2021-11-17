94d9e11894779f1801c874797762b0d9d5578b20
kurisuh <christofer.heyer@tu-dortmund.de>
2021-11-17 12:30:15 +0100

    Maybe Fix import Issues

--- src/app/viewer/viewer.module.ts
+++ src/app/viewer/viewer.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common'
 import { CodeViewerComponent } from './code-viewer/code-viewer.component'
 import { MonacoEditorModule } from 'ngx-monaco-editor'
 import { FormsModule } from '@angular/forms'
-import { OverlayModule } from "@angular/cdk/overlay";
+import { OverlayModule } from '@angular/cdk/overlay'
 import { UnityViewerComponent } from './unity-viewer/unity-viewer.component'
 import { UnityModule } from '../unity/unity.module'
 import { TerminalViewerComponent } from './terminal-viewer/terminal-viewer.component'
-import { ImageViewerComponent } from './image-viewer/image-viewer.component';
-import {MainViewerComponent} from './main-viewer/main-viewer.component';
-import {TabComponent} from 'src/app/viewer/tab/tab.component';
-import {TabGroupComponent} from './tab-group/tab-group.component';
-import {AngularSplitModule} from 'angular-split';
-import { HintViewerComponent } from './hint-viewer/hint-viewer.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component'
+import { MainViewerComponent } from './main-viewer/main-viewer.component'
+import { TabComponent } from 'src/app/viewer/tab/tab.component'
+import { TabGroupComponent } from './tab-group/tab-group.component'
+import { AngularSplitModule } from 'angular-split'
+import { HintViewerComponent } from './hint-viewer/hint-viewer.component'
 
 /**
  * Module used for all the Viewer-Components.
@@ -35,10 +35,23 @@ import { HintViewerComponent } from './hint-viewer/hint-viewer.component';
     MainViewerComponent,
     TabComponent,
     TabGroupComponent,
-    HintViewerComponent
+    HintViewerComponent,
   ],
-  imports: [CommonModule, MonacoEditorModule, FormsModule, UnityModule, AngularSplitModule, OverlayModule],
-  exports: [CodeViewerComponent, UnityViewerComponent, TerminalViewerComponent, ImageViewerComponent, MainViewerComponent],
+  imports: [
+    CommonModule,
+    MonacoEditorModule,
+    FormsModule,
+    UnityModule,
+    AngularSplitModule,
+    OverlayModule,
+  ],
+  exports: [
+    CodeViewerComponent,
+    UnityViewerComponent,
+    TerminalViewerComponent,
+    ImageViewerComponent,
+    MainViewerComponent,
+  ],
 })
 export class ViewerModule {
   constructor() {}
