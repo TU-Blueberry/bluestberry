763f1703b3870105b2568eacdf65eaaa672034d6
TimHal <tim.hallyburton@tu-dortmund.de>
2021-12-20 09:24:59 +0100

    [add] endless polyfills and hacks to use this old vscode lib

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,5 +1,4 @@
-const path = require('path');
-
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = {
   module: {
     rules: [
@@ -9,13 +8,30 @@ module.exports = {
       }
     ],
   },
-  target: "node",
+  plugins: [new NodePolyfillPlugin()]
+  // target: "node",
   // Do not change this!
   // Finding this shit took me more than 2 weeks
   // https://github.com/TypeFox/monaco-languageclient/blob/master/CHANGELOG.md#breaking-changes-1 (scroll to 0.7.0)
+  /**
   resolve: {
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+      "os": false,
+      "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+    } ,
     alias: {
-        'vscode': path.resolve(__dirname, 'node_modules/monaco-languageclient/lib/vscode-compatibility')
-    }
+        // 'vscode': require.resolve('monaco-languageclient/lib/vscode-compatibility')
+        'vscode': 'monaco-languageclient/lib/vscode-compatibility'
+      }
   }
+  */
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'ts-polyfill';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
