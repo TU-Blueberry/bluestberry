74f641f8d464de2ea43cc75feb76b0435d7ac30e
TimHal <tim.hallyburton@tu-dortmund.de>
2022-02-23 13:44:17 +0100

    import required 3rd party modules

--- src/app/viewer/viewer.module.ts
+++ src/app/viewer/viewer.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { CodeViewerComponent } from './code-viewer/code-viewer.component'
-import { MonacoEditorModule } from 'ngx-monaco-editor'
+
 import { FormsModule } from '@angular/forms'
 import { OverlayModule } from '@angular/cdk/overlay'
+import { MatSortModule } from '@angular/material/sort'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MonacoEditorModule } from 'ngx-monaco-editor'
+import { CodeViewerComponent } from './code-viewer/code-viewer.component'
 import { UnityViewerComponent } from './unity-viewer/unity-viewer.component'
 import { UnityModule } from '../unity/unity.module'
 import { TerminalViewerComponent } from './terminal-viewer/terminal-viewer.component'
 import { ImageViewerComponent } from './image-viewer/image-viewer.component'
 import { MainViewerComponent } from './main-viewer/main-viewer.component'
 import { AngularSplitModule } from 'angular-split'
-import { MatSortModule } from '@angular/material/sort'
+
 import { HintViewerComponent } from './hint-viewer/hint-viewer.component'
 import { FilesystemModule } from '../filesystem/filesystem.module'
 import { MarkdownViewerComponent } from "./markdown-viewer/markdown-viewer.component";
@@ -18,8 +22,14 @@ import { MarkdownModule } from "ngx-markdown";
 import { TabModule } from 'src/app/tab/tab.module';
 import {TabGroupFacadeComponent} from 'src/app/viewer/tab-group-facade/tab-group-facade.component';
 import { TableViewerComponent } from './table-viewer/table-viewer.component';
-import { PblNgridModule, PblNgridRegistryService } from '@pebula/ngrid';
+import { PblNgridConfigService, PblNgridModule } from '@pebula/ngrid';
+import { PblNgridTargetEventsModule } from '@pebula/ngrid/target-events';
 import { PlotlyViewerComponent } from './plotly-viewer/plotly-viewer.component';
+import { PblNgridMatSortModule } from '@pebula/ngrid-material/sort';
+import { PblNgridTransposeModule } from '@pebula/ngrid/transpose';
+import { PblNgridStatePluginModule } from '@pebula/ngrid/state';
+import { PblNgridDragModule } from '@pebula/ngrid/drag';
+
 
 /**
  * Module used for all the Viewer-Components.
@@ -57,7 +67,15 @@ import { PlotlyViewerComponent } from './plotly-viewer/plotly-viewer.component';
     MarkdownModule,
     TabModule,
     MatSortModule,
-    PblNgridModule
+    BrowserAnimationsModule,
+    PblNgridModule,
+    PblNgridTargetEventsModule,
+    PblNgridTargetEventsModule,
+    PblNgridMatSortModule,
+    PblNgridTransposeModule,
+    PblNgridStatePluginModule,
+    PblNgridDragModule.withDefaultTemplates()
+    
   ],
   exports: [
     CodeViewerComponent,
@@ -69,5 +87,7 @@ import { PlotlyViewerComponent } from './plotly-viewer/plotly-viewer.component';
   ],
 })
 export class ViewerModule {
-  constructor() {}
+  constructor(config: PblNgridConfigService) {
+    config.set('targetEvents', {autoEnable: true});
+  }
 }
