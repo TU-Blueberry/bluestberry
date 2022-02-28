f5573449b95158013082d0a2715b0f5ae80ae5c8
ti-kat <tim.katzke@tu-dortmund.de>
2022-02-28 10:18:19 +0100

    Fixed error message filter for deployed version

--- src/app/viewer/terminal-viewer/terminal-viewer.filtered-errors.ts
+++ src/app/viewer/terminal-viewer/terminal-viewer.filtered-errors.ts
@@ -1,5 +1,6 @@
 /** Prefixes of error messages considered unnecessary */
 export const filteredTerminalErrorPrefixes = [
-  '/lib/python3.9/site-packages/pandas/compat/__init__.py:117: UserWarning: Could not import the lzma module.',
+  '/lib/python3.9/site-packages/pandas/compat/__init__.py:117: UserWarning: Could not import the lzma module.', // For local dev
+  '/lib/python3.9/site-packages/pandas/compat/__init__.py:124: UserWarning: Could not import the lzma module.',
   '  warnings.warn(msg)',
 ]
