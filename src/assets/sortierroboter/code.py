de0dcaff7288bb6289035dfa772d7bd6149ad533
ti-kat <tim.katzke@tu-dortmund.de>
2022-03-09 15:26:55 +0100

    Fixed typo and improved initial code template

--- src/assets/sortierroboter/code.py
+++ src/assets/sortierroboter/code.py
@@ -1,1 +1,5 @@
-print("Herzlich willkommen bei "Project Blueberry")
+# Über das Fragezeichen-Symbol in der Aktionsleiste (linker Rand der Website)
+# können Sie eine kurze Informations-Tour zu dieser Lernplattform starten.
+
+# Dieser Code ist mittels des linken Buttons im unteren Bereich des Editors ausführbar.
+print("Herzlich willkommen bei Project Blueberry")
