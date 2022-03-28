63d5ccdfd6931120b27041e649a876df281a07f1
makoeni <maximilian.koenig@tu-dortmund.de>
2022-03-28 18:05:43 +0200

    Clarify some fields of config.ts

--- src/app/experience/model/config.ts
+++ src/app/experience/model/config.ts
@@ -2,7 +2,7 @@ import { ExperienceType } from "src/app/experience/model/experience-type";
 import { ViewSettings } from "src/app/viewer/model/view-settings";
 
 export interface Config {
-    open: { path: string, on: string, active: boolean }[],
+    open: { path: string, on: string, active: boolean }[], // info about each open tab
     uuid: string;
     name: string;
     type: ExperienceType;
@@ -11,10 +11,10 @@ export interface Config {
     hidden: string[]; // paths which will be hidden from the UI and (recursive)
     readonly: string[]; // paths which should be read only (recursive), e.g. lesson description
     modules: string[]; // paths to our custom python modules; will be set to hidden and read-only if specified
-    glossaryEntryPoint: string; // will be copied to global glossary scope (read only, recursive)
-    hintRoot: string;
-    preloadPythonLibs: string[];
-    tabinfo: string;
+    glossaryEntryPoint: string; // will be displayed together with all global entries (read only, recursive)
+    hintRoot: string; // location of the root.yml file for hints
+    preloadPythonLibs: string[]; // names of python libs which shall be preloaded
+    tabinfo: string; // folder which stores current content of special tabs, e.g. plotly
 
     // options we envisioned but didn't get to implement
     encrypted: string[]; // paths which will be encrypted upon export (config.json is always encrypted)
